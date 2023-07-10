555b5cab6f84161b37d3abfc536070fdc4872ceb
Endeyr <aaron.thompson42594@gmail.com>
2023-07-10 13:41:43 -0400

    styled card page

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -57,6 +57,13 @@ module.exports = {
 			sans: ['Helvetica', 'sans-serif'],
 			serif: ['Georgia', 'serif'],
 		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+		},
 		extend: {},
 	},
 	plugins: [
